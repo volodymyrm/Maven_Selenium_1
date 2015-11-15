1f1bf7e6fdce7fe521f9d49995a30dff8003e9bf
volodymyrm <vovych007@gmail.com>
2015-11-15 20:47:57 +0200

    Its Work

--- src/test/java/package_with_tests/Test1.java
+++ src/test/java/package_with_tests/Test1.java
@@ -1,12 +1,20 @@
 package package_with_tests;
 
-import org.juinit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test1 {
 
     @Test
     public void startWebDriver(){
 
+        WebDriver driver = new FirefoxDriver();
 
+        driver.navigate().to("http://Google.com");
+
+        driver.close();
     }
 
 }
