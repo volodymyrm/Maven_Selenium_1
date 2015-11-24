bd4c5c7bcd49ca412963055f333361d2ffb35f10
volodymyrm <vovych007@gmail.com>
2015-11-24 10:48:21 +0200

    WebDriver object

--- src/test/java/package_with_tests/Tests.java
+++ src/test/java/package_with_tests/Tests.java
@@ -10,30 +10,36 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import javax.swing.plaf.basic.BasicArrowButton;
+
 public class Tests {
 
+    WebDriver driver;
     Page eg;
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "\\Chromedriver\\chromedriver.exe");
-        eg = PageFactory.initElements(new ChromeDriver(), Page.class);
+       // eg = PageFactory.initElements(new ChromeDriver(), Page.class);
 
-        eg.open(Link.lnk);
-        eg.switchToFrame();
+        driver = new ChromeDriver();
+
+        //eg.open(Link.lnk);
+        //eg.switchToFrame();
+        driver.navigate().to(Link.lnk);
+        driver.switchTo().frame(0);
     }
 
     @After
     public void thearDown(){
-        eg.close();
+        driver.quit();
     }
 
     @Test
     public void testOne(){
+        Page eg = new Page(driver);
         eg.typefName("VovaVova*&*");
         eg.typelName("KHKHHYJJ*");
-        eg.typeEmail("email@email.com");
-       // eg.setDOB("20");
         eg.submitRegistration();
 
         /*try {
@@ -42,4 +48,13 @@ public class Tests {
             e.printStackTrace();
         }*/
     }
+
+  /*  @Test
+    public void testTwo(){
+        Page eg = new Page(driver);
+        eg.typefName("VVVVVVV66^^^");
+        eg.typelName("VVVVVVV66^^^");
+        eg.submitRegistration();
+        driver.close();
+    }*/
 }
