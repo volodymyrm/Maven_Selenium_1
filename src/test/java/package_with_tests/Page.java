5dbd20e2b3abe0684c145ef5f45df4028f1f8f82
volodymyrm <vovych007@gmail.com>
2015-11-25 10:58:49 +0200

    PageFactory object - select doesnt work

--- src/test/java/package_with_tests/Page.java
+++ src/test/java/package_with_tests/Page.java
@@ -1,19 +1,33 @@
 package package_with_tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Page {
 
     protected WebDriver driver;
     By fName = By.id("firstname");
     By lName = By.id("lastname");
+    By eMail = By.id("email");
+
+   /* WebElement day = driver.findElement(By.xpath("//div[@class = 'datepicker_day datepicker_field']/select"));
+    Select selectDay = new Select(day);*/
+
     By regButton = By.className("button");
+
+    By emailErrorMsg = By.xpath("//div[6]/div[@class='error_message']");
 
     public Page(WebDriver driver) {
         this.driver = driver;
     }
+
+ /*   public Page setDOB(String dayvalue){
+        selectDay.selectByValue(dayvalue);
+        return this;
+    }*/
+
     public void open(String url){
         driver.get(url);
     }
@@ -35,6 +49,12 @@ public class Page {
         driver.findElement(lName).sendKeys(fname);
         return this;
     }
+
+    public Page typeEmail(String email){
+        driver.findElement(eMail).sendKeys(email);
+        return this;
+    }
+
 
     public Page submitRegistration() {
         driver.findElement(regButton).click();
--- src/test/java/package_with_tests/Tests.java
+++ src/test/java/package_with_tests/Tests.java
@@ -1,31 +1,23 @@
 package package_with_tests;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import javax.swing.plaf.basic.BasicArrowButton;
-
 public class Tests {
 
-   // WebDriver driver;
     Page eg;
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "\\Chromedriver\\chromedriver.exe");
         eg = PageFactory.initElements(new ChromeDriver(), Page.class);
-       // page.open("http://google.co.in/");
-        //driver = new ChromeDriver();
-        eg.open(Link.lnk);
-        // driver.navigate().to(Link.lnk);
+        eg.open("http://m-stg-admin.williamhillcasino.com/register2");
         eg.switchToFrame();
+        //driver.navigate().to(Link.lnk);
+        //driver.switchTo().frame(0);
     }
 
     @After
@@ -37,13 +29,14 @@ public class Tests {
     public void testOne(){
         eg.typefName("VovaVova*&*");
         eg.typelName("KHKHHYJJ*");
+     //   eg.setDOB("20");
         eg.submitRegistration();
 
-        /*try {
+        try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
   /*  @Test
