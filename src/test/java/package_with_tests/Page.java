2463256e4759fde1b926f95f1e049385562b3b5c
volodymyrm <vovych007@gmail.com>
2015-11-25 15:55:16 +0200

    All page - inputs described

--- src/test/java/package_with_tests/Page.java
+++ src/test/java/package_with_tests/Page.java
@@ -12,6 +12,15 @@ public class Page {
     By lName = By.id("lastname");
     By eMail = By.id("email");
     By regButton = By.className("button");
+    By city = By.id("city");
+    By address = By.id("address");
+    By zip = By.id("zip");
+    By cellPhone = By.id("cellphone");
+    By uName = By.id("userName");
+    By passWord = By.id("password");
+    By passwordConfirm = By.id("passwordConfirm");
+    By yearsConfirm = By.xpath("//*[@id='registrationForm']/div[19]");
+    By emailSubscription = By.xpath("//*[@id='registrationForm']/div[20]");
     By emailErrorMsg = By.xpath("//div[6]/div[@class='error_message']");
 
     public Page(WebDriver driver) {
@@ -67,9 +76,61 @@ public class Page {
         return this;
     }
 
+    public Page setDepositLimit(String dopositvalue){
+        WebElement deposit = driver.findElement(By.xpath("//*[@id='registrationForm']/div[16]/select"));
+        Select selectCountry = new Select(deposit);
+        selectCountry.selectByValue(dopositvalue);
+        return this;
+    }
+
 
     public Page typeEmail(String email){
         driver.findElement(eMail).sendKeys(email);
+        return this;
+    }
+
+    public Page typeCity(String cityvalue){
+        driver.findElement(city).sendKeys(cityvalue);
+        return this;
+    }
+
+    public Page typeAddress(String addressvalue){
+        driver.findElement(address).sendKeys(addressvalue);
+        return this;
+    }
+
+    public Page typeZip(String zipvalue){
+        driver.findElement(zip).sendKeys(zipvalue);
+        return this;
+    }
+
+    public Page typeCellphone(String cellphonevalue){
+        driver.findElement(cellPhone).sendKeys(cellphonevalue);
+        return this;
+    }
+
+    public Page typeUname(String unamevalue){
+        driver.findElement(uName).sendKeys(unamevalue);
+        return this;
+    }
+
+    public Page typePassword(String passwordvalue){
+        driver.findElement(passWord).sendKeys(passwordvalue);
+        return this;
+    }
+
+    public Page typeConfirmPass(String confirmpassvalue){
+        driver.findElement(passwordConfirm).sendKeys(confirmpassvalue);
+        return this;
+    }
+
+    public Page submitYearsConfirm(){
+        driver.findElement(yearsConfirm).click();
+        return this;
+    }
+
+    public Page submitEmailSubscription(){
+        driver.findElement(emailSubscription).click();
         return this;
     }
 
--- src/test/java/package_with_tests/Tests.java
+++ src/test/java/package_with_tests/Tests.java
@@ -26,16 +26,26 @@ public class Tests {
 
     @Test
     public void testOne(){
-        eg.typefName("VovaVova*&*");
-        eg.typelName("KHKHHYJJ*");
-        eg.setTitle("Mr");
-        eg.typeEmail("sdfsdf@sdfds");
-        eg.setCountry("GB");
-        eg.setDOB("20", "06", "1990");
+        eg.typefName("HJGFjgj3*");
+        eg.typelName("HJGFjgj3*");
+        //eg.setDOB("20", "06", "1990");
+        //eg.setTitle("Mr");
+        eg.typeEmail("HJGFjgj3*");
+        //eg.setCountry("GB");
+        eg.typeCity("HJGFjgj3*");
+        eg.typeAddress("HJGFjgj3*");
+        eg.typeZip("1234567890123");
+        eg.typeCellphone("HJGFjgj3*");
+        eg.typeUname("HJGFjgj3*");
+        eg.typePassword("HJGFjgj3*");
+        eg.typeConfirmPass("HJGFjgj3*");
+        //eg.setDepositLimit("0");
+        //eg.submitYearsConfirm();
+        eg.submitEmailSubscription();
         eg.submitRegistration();
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(25000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
